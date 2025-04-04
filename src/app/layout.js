077e460b5f444594aef491dcca09b87d3b26f526
Marina <marina.crespo@valtech.com>
2025-04-04 14:22:32 -0300

    theme fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,12 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased flex flex-col h-screen justify-evenly w-screen m-auto p-10 md:p-0 text-center bg-[url(/bg-intro-desktop.png)] bg-[#FF7A7A] bg-cover bg-center`}
-      >
-        {children}
-      <footer className="text-center mt-4">Challenge by <a href="https://www.frontendmentor.io/home" className="text-[#6055A5]">Frontend Mentor</a>. Coded by <a href="https://github.com/chapucrespo" className="text-[#6055A5]">Chapu Crespo</a></footer>
-      </body>
+        <body
+          className={`${poppins.variable} antialiased flex flex-col h-screen justify-evenly w-screen m-auto p-10 xl:p-0 text-center bg-[url(/bg-intro-desktop.png)] bg-[#FF7A7A] bg-cover bg-center`}
+        >
+          {children}
+        <footer className="text-center mt-4">Challenge by <a href="https://www.frontendmentor.io/home" className="text-[#6055A5]">Frontend Mentor</a>. Coded by <a href="https://github.com/chapucrespo" className="text-[#6055A5]">Chapu Crespo.</a></footer>
+        </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
 export default function Home() {
   return ( 
-      <main className="flex flex-col lg:flex-row lg:w-screen lg:justify-around">
-        <div className="flex flex-col justify-center">
-          <p className="text-2xl font-bold">Learn to code by watching others</p>
-          <p className="lg:w-100">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </p>
+      <main className="flex flex-col lg:flex-row lg:w-screen lg:justify-center">
+        <div className="flex flex-col justify-center lg:w-170 lg:text-left">
+          <p className="text-2xl font-bold lg:text-6xl">Learn to code by watching others</p>
+          <p className="lg:mt-4">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </p>
         </div>
         <div className="lg:w-140">
-          <div className="bg-[#6055A5] rounded-2xl h-25 lg:h-12 flex flex-col lg:flex-row items-center justify-center mt-6 shadow-(--shadow)"><p className="font-bold">Try it free 7 days </p> then $20/mo. thereafter</div>
-          <form>
-            <div className="bg-white mt-5 rounded-2xl flex flex-col items-center shadow-(--shadow) lg:p-6">
-              <input type="text" name="name" placeholder="First Name" required className="text-black border w-60 lg:w-full p-3 mt-3 border-[#B9B6D3] rounded"></input>
-              <input type="text" name="lastName" placeholder="Last Name" required className="text-black border w-60 lg:w-full p-3 mt-2 border-[#B9B6D3] rounded"></input>
-              <input type="text" name="email" required placeholder="Email" className="text-black border w-60 lg:w-full p-3 mt-2 border-[#B9B6D3] rounded"></input>
-              <input type="text" name="password" required placeholder="Password" className="text-black border w-60 lg:w-full p-3 mt-2 border-[#B9B6D3] rounded"></input>
-              <button type="submit" className="bg-[#38CC8C] mt-5 rounded w-60 h-12 font-bold shadow-(--gshadow) lg:w-full"> CLAIM YOUR FREE TRIAL </button>
-              <p className="text-[#B9B6D3] text-xs p-3">By clicking the button you are agreeing to our <a href="www.google.com" className="text-amber-600">Terms and services</a></p>
+          <div className="bg-[var(--blue)] rounded-2xl h-25 lg:h-12 flex flex-col lg:flex-row items-center justify-center mt-6 shadow-(--shadow)"><p><span className="font-bold">Try it free 7 days</span> then $20/mo. thereafter</p></div>
+          <form className="lg:h-130">
+            <div className="bg-white mt-5 rounded-2xl flex flex-col items-center shadow-(--shadow) lg:p-10">
+              <input type="text" name="name" placeholder="First Name" required className="text-black border w-60 lg:w-full p-3 mt-3 border-[var(--grayish-blue)] rounded"></input>
+              <input type="text" name="lastName" placeholder="Last Name" required className="text-black border w-60 lg:w-full p-3 mt-2 border-[var(--grayish-blue)] rounded"></input>
+              <input type="text" name="email" required placeholder="Email" className="text-black border w-60 lg:w-full p-3 mt-2 border-[var(--grayish-blue)] rounded"></input>
+              <input type="text" name="password" required placeholder="Password" className="text-black border w-60 lg:w-full p-3 mt-2 border-[var(--grayish-blue)] rounded"></input>
+              <button type="submit" className="bg-[var(--green)] mt-5 rounded w-60 h-12 font-bold shadow-(--gshadow) lg:w-full hover:bg-[var(--green-shadow)]"> CLAIM YOUR FREE TRIAL </button>
+              <p className="text-[var(--grayish-blue)] text-xs p-3">By clicking the button you are agreeing to our <a href="www.google.com" className="text-[var(--orange)]">Terms and services</a></p>
             </div>
           </form>
         </div>
